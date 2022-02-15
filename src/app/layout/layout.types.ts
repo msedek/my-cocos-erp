de952c79977954ea5eac246a897d3695610ed374
Miguel Sedek <miguelsedek@gmail.com>
2022-02-14 23:02:03 -0300

    cleaning

--- src/app/layout/layout.types.ts
+++ src/app/layout/layout.types.ts
@@ -1,1 +1,14 @@
-export type Layout = 'classy';
+export type Layout =
+    | 'empty'
+    // Horizontal
+    | 'centered'
+    | 'enterprise'
+    | 'material'
+    | 'modern'
+    // Vertical
+    | 'classic'
+    | 'classy'
+    | 'compact'
+    | 'dense'
+    | 'futuristic'
+    | 'thin';
